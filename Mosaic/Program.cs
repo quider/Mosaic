f50c978ebab46abce901e840a8c859507871da5e
akozlowski <adrian.kozlowski@outlook.com>
2015-05-19 01:00:02 +0200

    Added log4net

--- Mosaic/Program.cs
+++ Mosaic/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -7,12 +9,28 @@ namespace Mosaic
 {
     static class Program
     {
+        static ILog log = LogManager.GetLogger(typeof(Program));
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            
+            log.InfoFormat("Start application. Username: {0}, System directory: {1}, System Version {2}, Processor count: {3}, OSVersion: {4}, MachineName: {5}, Is 64Bit process: {6}, is 64 bit os: {7}, Working set: {8}",
+                Environment.UserName, 
+                Environment.SystemDirectory,
+                Environment.Version,
+                Environment.ProcessorCount,
+                Environment.OSVersion,
+                Environment.MachineName,
+                Environment.Is64BitProcess,
+                Environment.Is64BitOperatingSystem,
+                Environment.WorkingSet);
+
+            log.InfoFormat("Culture info:{0}", CultureInfo.CurrentCulture);
+
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
