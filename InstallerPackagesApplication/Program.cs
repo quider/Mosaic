6b7bb1648e33558e6c25c2fa2de6f687207943e2
akozlowski <adrian.kozlowski@outlook.com>
2015-05-27 00:08:16 +0200

    Added new project InstallerPackagesApplication

---
+++ InstallerPackagesApplication/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstallerPackagesApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+            Console.WriteLine(args[2]);
+            Console.WriteLine(args[3]);
+            Assembly assembly = Assembly.LoadFile(Path.Combine(args[1],"Bin",args[3],"Mosaic.exe"));
+            var version = assembly.GetName().Version;
+
+            string filename = args[0];
+            string ftpServerIP = "ftp.quider.pl/";
+            string ftpUserName = "quiderpl";
+            string ftpPassword = "!nichuja.1990";
+
+            FileInfo objFile = new FileInfo(filename+".msi");
+            objFile.MoveTo(filename+"_"+args[3]+"_"+args[2]+"_"+version.ToString(4)+".msi");
+            FtpWebRequest objFTPRequest;
+
+            // Create FtpWebRequest object 
+            objFTPRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + ftpServerIP + "/public_html/mosaic/" + objFile.Name));
+
+            // Set Credintials
+            objFTPRequest.Credentials = new NetworkCredential(ftpUserName, ftpPassword);
+
+            // By default KeepAlive is true, where the control connection is 
+            // not closed after a command is executed.
+            objFTPRequest.KeepAlive = false;
+
+            // Set the data transfer type.
+            objFTPRequest.UseBinary = true;
+
+            // Set content length
+            objFTPRequest.ContentLength = objFile.Length;
+
+            // Set request method
+            objFTPRequest.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // Set buffer size
+            int intBufferLength = 16 * 1024;
+            byte[] objBuffer = new byte[intBufferLength];
+
+            // Opens a file to read
+            FileStream objFileStream = objFile.OpenRead();
+
+            try
+            {
+                // Get Stream of the file
+                Stream objStream = objFTPRequest.GetRequestStream();
+
+                int len = 0;
+
+                while ((len = objFileStream.Read(objBuffer, 0, intBufferLength)) != 0)
+                {
+                    // Write file Content 
+                    objStream.Write(objBuffer, 0, len);
+
+                }
+
+                objStream.Close();
+                objFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
