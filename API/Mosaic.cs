4828ba9644eaebc4447cb7bd745c6e9c81519962
akozlowski <adrian.kozlowski@outlook.com>
2015-11-27 18:44:25 +0100

    API project added

---
+++ API/Mosaic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public abstract class Mosaic
+    {
+        public virtual void CalculateMosaic(object sender, DoWorkEventArgs e)
+        {
+        
+        }
+    }
+}
