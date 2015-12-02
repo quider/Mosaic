736e57594b794acd4b4407aa71755f9253630fd3
akozlowski <adrian.kozlowski@outlook.com>
2015-12-02 23:04:27 +0100

    Version application

---
+++ ReadVersion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dllForVersionExamination = args[0];
+            Assembly assembly = Assembly.LoadFile(dllForVersionExamination);
+            var version = assembly.GetName().Version;
+            Console.WriteLine("Version: "+version.ToString());
+        }
+    }
+}
